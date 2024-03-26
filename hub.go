13bd6b3b754d846c76b03ca2bb447eb347f47f35
ksCaesar <x246libra@hotmail.com>
2024-03-26 09:49:45 +0800

    refactor: hub

--- hub.go
+++ hub.go
@@ -5,110 +5,126 @@ import (
 	"sync/atomic"
 )
 
-func NewHub[Adapter any](stop func(*Adapter) error) *Hub[Adapter] {
-	return &Hub[Adapter]{
-		adapters:    make(map[string]*Adapter),
-		stopAdapter: stop,
+func NewHub[T any](stopObj func(*T) error) *Hub[T] {
+	return &Hub[T]{
+		collections: make(map[string]*T),
+		stopObj:     stopObj,
 	}
 }
 
-// Hub can manage multiple adapters.
+// Hub
 //
 //	concurrencyQty controls how many tasks can run simultaneously,
 //	preventing resource usage or avoid frequent context switches.
-type Hub[Adapter any] struct {
-	adapters       map[string]*Adapter // Identifier:Adapter
-	stopAdapter    func(*Adapter) error
+type Hub[T any] struct {
+	collections    map[string]*T // Identifier:T
 	concurrencyQty int
 	mu             sync.RWMutex
-	isStop         atomic.Bool
+
+	stopObj func(*T) error
+	isStop  atomic.Bool
 }
 
-func (hub *Hub[Adapter]) JoinAdapter(name string, adapter *Adapter) error {
+func (hub *Hub[T]) Join(key string, obj *T) error {
 	hub.mu.Lock()
 	defer hub.mu.Unlock()
 	if hub.isStop.Load() {
 		return ErrorWrapWithMessage(ErrClosed, "Artifex hub")
 	}
-	_, found := hub.adapters[name]
+
+	_, found := hub.collections[key]
 	if found {
-		return ErrorWrapWithMessage(ErrInvalidParameter, "duplicated adapter=%v", name)
+		return ErrorWrapWithMessage(ErrInvalidParameter, "duplicated key=%v", key)
 	}
-	hub.adapters[name] = adapter
+	hub.collections[key] = obj
 	return nil
 }
 
-func (hub *Hub[Adapter]) RemoveAdapter(name string) {
+func (hub *Hub[T]) Remove(key string) {
 	hub.mu.Lock()
 	defer hub.mu.Unlock()
-	adapter, found := hub.adapters[name]
+
+	obj, found := hub.collections[key]
 	if !found {
 		return
 	}
-	delete(hub.adapters, name)
-	hub.stopAdapter(adapter)
+	delete(hub.collections, key)
+	hub.stopObj(obj)
 }
 
-func (hub *Hub[Adapter]) UpdateAdapterName(oldName string, updateName func(*Adapter) (freshName string)) error {
+func (hub *Hub[T]) UpdateKeyAndObj(oldKey string, update func(*T) (freshKey string)) error {
 	hub.mu.Lock()
 	defer hub.mu.Unlock()
-
 	if hub.isStop.Load() {
 		return ErrorWrapWithMessage(ErrClosed, "Artifex hub")
 	}
 
-	adapter, found := hub.adapters[oldName]
+	obj, found := hub.collections[oldKey]
 	if !found {
-		return ErrorWrapWithMessage(ErrNotFound, "adapter=%v not exist in hub", oldName)
+		return ErrorWrapWithMessage(ErrNotFound, "key=%v not exist in hub", oldKey)
 	}
-	freshName := updateName(adapter)
-	hub.adapters[freshName] = adapter
-	delete(hub.adapters, oldName)
+	freshKey := update(obj)
+	hub.collections[freshKey] = obj
+	delete(hub.collections, oldKey)
 	return nil
 }
 
-func (hub *Hub[Adapter]) Stop() {
-	hub.mu.Lock()
-	defer hub.mu.Unlock()
-
-	if hub.isStop.Load() {
-		return
-	}
-	hub.isStop.Store(true)
-
-	for name, adapter := range hub.adapters {
-		delete(hub.adapters, name)
-		hub.stopAdapter(adapter)
-	}
-}
-
-func (hub *Hub[Adapter]) DoSync(action func(*Adapter) (stop bool)) {
-	if hub.isStop.Load() {
-		return
-	}
+func (hub *Hub[T]) UpdateByKey(key string, update func(*T)) error {
 	hub.mu.RLock()
 	defer hub.mu.RUnlock()
+	if hub.isStop.Load() {
+		return ErrorWrapWithMessage(ErrClosed, "Artifex hub")
+	}
 
-	for _, adapter := range hub.adapters {
-		stop := action(adapter)
+	obj, found := hub.collections[key]
+	if !found {
+		return ErrorWrapWithMessage(ErrNotFound, "key=%v not exist in hub", key)
+	}
+	update(obj)
+	return nil
+}
+
+func (hub *Hub[T]) StopAll() {
+	hub.mu.Lock()
+	defer hub.mu.Unlock()
+	if hub.isStop.Load() {
+		return
+	}
+
+	hub.isStop.Store(true)
+	for key, obj := range hub.collections {
+		delete(hub.collections, key)
+		hub.stopObj(obj)
+	}
+}
+
+func (hub *Hub[T]) DoSync(action func(*T) (stop bool)) {
+	hub.mu.RLock()
+	defer hub.mu.RUnlock()
+	if hub.isStop.Load() {
+		return
+	}
+
+	for _, obj := range hub.collections {
+		stop := action(obj)
 		if stop {
 			break
 		}
 	}
 }
 
-func (hub *Hub[Adapter]) DoAsync(action func(*Adapter)) {
+func (hub *Hub[T]) DoAsync(action func(*T)) {
+	hub.mu.RLock()
+	defer hub.mu.RUnlock()
 	if hub.isStop.Load() {
 		return
 	}
-	hub.mu.RLock()
-	defer hub.mu.RUnlock()
 
 	var bucket chan struct{}
-	for _, adp := range hub.adapters {
-		adapter := adp
+	for _, v := range hub.collections {
+		obj := v
 		if hub.concurrencyQty <= 0 {
-			go action(adapter)
+			go action(obj)
 			continue
 		}
 
@@ -118,16 +134,16 @@ func (hub *Hub[Adapter]) DoAsync(action func(*Adapter)) {
 			defer func() {
 				<-bucket
 			}()
-			action(adapter)
+			action(obj)
 		}()
 	}
 }
 
-func (hub *Hub[Adapter]) FindAdapter(filter func(*Adapter) bool) (adapter *Adapter, found bool) {
-	var target *Adapter
-	hub.DoSync(func(adapter *Adapter) (stop bool) {
-		if filter(adapter) {
-			target = adapter
+func (hub *Hub[T]) Find(filter func(*T) bool) (obj *T, found bool) {
+	var target *T
+	hub.DoSync(func(obj *T) (stop bool) {
+		if filter(obj) {
+			target = obj
 			return true
 		}
 		return false
@@ -135,42 +151,42 @@ func (hub *Hub[Adapter]) FindAdapter(filter func(*Adapter) bool) (adapter *Adapt
 	return target, target != nil
 }
 
-func (hub *Hub[Adapter]) FindAdapterByName(name string) (adapter *Adapter, found bool) {
+func (hub *Hub[T]) FindByKey(key string) (obj *T, found bool) {
 	hub.mu.RLock()
 	defer hub.mu.RUnlock()
-	adapter, found = hub.adapters[name]
+	obj, found = hub.collections[key]
 	return
 }
 
-func (hub *Hub[Adapter]) FindAdapters(filter func(*Adapter) bool) (adapters []*Adapter, found bool) {
-	adapters = make([]*Adapter, 0)
-	hub.DoSync(func(adapter *Adapter) bool {
-		if filter(adapter) {
-			adapters = append(adapters, adapter)
+func (hub *Hub[T]) FindAll(filter func(*T) bool) (all []*T, found bool) {
+	all = make([]*T, 0)
+	hub.DoSync(func(obj *T) bool {
+		if filter(obj) {
+			all = append(all, obj)
 		}
 		return false
 	})
-	return adapters, len(adapters) > 0
+	return all, len(all) > 0
 }
 
-func (hub *Hub[Adapter]) FindAdaptersByName(names []string) (adapters []*Adapter, found bool) {
+func (hub *Hub[T]) FindAllByKey(keys []string) (all []*T, found bool) {
 	hub.mu.RLock()
 	defer hub.mu.RUnlock()
 
-	adapters = make([]*Adapter, 0)
-	for i := 0; i < len(names); i++ {
-		adapter, ok := hub.adapters[names[i]]
+	all = make([]*T, 0)
+	for i := 0; i < len(keys); i++ {
+		obj, ok := hub.collections[keys[i]]
 		if ok {
-			adapters = append(adapters, adapter)
+			all = append(all, obj)
 		}
 	}
-	return adapters, len(adapters) > 0
+	return all, len(all) > 0
 }
 
-func (hub *Hub[Adapter]) Count(filter func(*Adapter) bool) int {
+func (hub *Hub[T]) Count(filter func(*T) bool) int {
 	cnt := 0
-	hub.DoSync(func(adapter *Adapter) bool {
-		if filter(adapter) {
+	hub.DoSync(func(obj *T) bool {
+		if filter(obj) {
 			cnt++
 		}
 		return false
@@ -178,13 +194,13 @@ func (hub *Hub[Adapter]) Count(filter func(*Adapter) bool) int {
 	return cnt
 }
 
-func (hub *Hub[Adapter]) Total() int {
+func (hub *Hub[T]) Total() int {
 	hub.mu.RLock()
 	defer hub.mu.RUnlock()
-	return len(hub.adapters)
+	return len(hub.collections)
 }
 
-func (hub *Hub[Adapter]) SetConcurrencyQty(concurrencyQty int) {
+func (hub *Hub[T]) SetConcurrencyQty(concurrencyQty int) {
 	if hub.isStop.Load() {
 		return
 	}
