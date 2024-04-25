40f51a5130ff31f2b8cce17159950c3a56d69bb5
ksCaesar <x246libra@hotmail.com>
2024-04-25 10:30:48 +0800

    refactor: shutdown method name

--- shutdown.go
+++ shutdown.go
@@ -13,31 +13,31 @@ func NewShutdown() *Shutdown {
 	notify := make(chan os.Signal, 2)
 	signal.Notify(notify, syscall.SIGINT, syscall.SIGTERM)
 	return &Shutdown{
-		done:      make(chan struct{}),
-		sysSignal: notify,
-		Logger:    DefaultLogger(),
+		done:   make(chan struct{}),
+		osSig:  notify,
+		Logger: DefaultLogger(),
 	}
 }
 
 type Shutdown struct {
-	done      chan struct{}
-	cancel    context.CancelCauseFunc
-	sysSignal chan os.Signal
-	Logger    Logger
+	done   chan struct{}
+	cancel context.CancelCauseFunc
+	osSig  chan os.Signal
+	Logger Logger
 
 	stopQty     int
 	names       []string
 	stopActions []func() error
 }
 
-func (s *Shutdown) SetStopAction(name string, action func() error) *Shutdown {
+func (s *Shutdown) StopService(name string, action func() error) *Shutdown {
 	s.stopQty++
 	s.names = append(s.names, name)
 	s.stopActions = append(s.stopActions, action)
 	return s
 }
 
-func (s *Shutdown) NotifyStop(cause error) {
+func (s *Shutdown) Notify(cause error) {
 	select {
 	case <-s.done:
 		return
@@ -46,11 +46,11 @@ func (s *Shutdown) NotifyStop(cause error) {
 	}
 }
 
-func (s *Shutdown) Wait() {
-	<-s.done
+func (s *Shutdown) WaitFinish() chan struct{} {
+	return s.done
 }
 
-func (s *Shutdown) Listen(ctx context.Context) {
+func (s *Shutdown) Serve(ctx context.Context) {
 	defer close(s.done)
 
 	if ctx == nil {
@@ -59,15 +59,15 @@ func (s *Shutdown) Listen(ctx context.Context) {
 	ctx, s.cancel = context.WithCancelCause(ctx)
 
 	select {
-	case sig := <-s.sysSignal:
-		s.Logger.Info("receive signal: %v", sig)
+	case sig := <-s.osSig:
+		s.Logger.Info("recv os signal: %v", sig)
 
 	case <-ctx.Done():
 		err := context.Cause(ctx)
 		if errors.Is(err, context.Canceled) {
-			s.Logger.Info("receive go context")
+			s.Logger.Info("recv go context")
 		} else {
-			s.Logger.Error("receive go context: %v", err)
+			s.Logger.Error("recv go context: %v", err)
 		}
 	}
 
